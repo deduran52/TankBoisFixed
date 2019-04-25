d797b0b880ff2a3a0e4ed612916071b3bac55e2d
Jerrik Waugh <jcwaugh@nmsu.edu>
2019-04-25 10:55:48 -0600

    Destruct terrain added
    The game is slowed but I am going to try to figure out how to increase FPS. I think mountain is too big. Projectile script changed grid added

--- Functional Tank Game/Assets/Scripts/ProjectileCollision.cs
+++ Functional Tank Game/Assets/Scripts/ProjectileCollision.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ProjectileCollision : MonoBehaviour
 {
@@ -15,16 +16,28 @@ public class ProjectileCollision : MonoBehaviour
 
     /* Delay timer */
     public float delayTimer;
+<<<<<<< Updated upstream
 
     public GameObject explosion; // drag your explosion prefab here
+=======
+    public GameObject explosion; // drag your explosion prefab here
+    public GameObject tilemapGameObject;
+
+    Tilemap tilemap;
+>>>>>>> Stashed changes
 
     public void Start()
     {
         player1 = GameObject.FindWithTag("PlayerTank1").GetComponent<PlayerTank1>();
         player2 = GameObject.FindWithTag("PlayerTank2").GetComponent<PlayerTank2>();
         camera1 = GameObject.FindWithTag("MainCamera").GetComponent<CameraPan>();
+        tilemap = GameObject.FindWithTag("Ground").GetComponent<Tilemap>();
 
         IsDestroyed = true;
+
+       
+        tilemap = tilemapGameObject.GetComponent<Tilemap>();
+
     }
 
     private void Update()
@@ -56,6 +69,7 @@ public class ProjectileCollision : MonoBehaviour
     
     void OnCollisionEnter2D(Collision2D other)
     {
+        Vector2 hitPosition = Vector2.zero;
         if (other.gameObject.CompareTag("Ground"))
         {
 
@@ -63,6 +77,7 @@ public class ProjectileCollision : MonoBehaviour
             Destroy(expl, 1); // delete the explosion after 1 seconds
             Destroy(gameObject, Impactdelay);
             IsDestroyed = true;
+<<<<<<< Updated upstream
         }
 
         else if(other.gameObject.CompareTag("Wall"))
@@ -89,9 +104,77 @@ public class ProjectileCollision : MonoBehaviour
             Destroy(expl, 1); // delete the explosion after 1 seconds
             Destroy(gameObject, Impactdelay);
             IsDestroyed = true;
+=======
+
+
+                foreach (ContactPoint2D hit in other.contacts)
+                {
+                    hitPosition.x = hit.point.x;
+                    hitPosition.y = hit.point.y;
+                    TileDestructionLoop(hitPosition);
+                    
+                }
+            
         }
 
+
+
+        else if (other.gameObject.CompareTag("Wall"))
+        {
+
+            GameObject expl = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
+            Destroy(expl, 1); // delete the explosion after 3 seconds
+            Destroy(gameObject, Impactdelay);
+            IsDestroyed = true;
+        }
+        else if (other.gameObject.CompareTag("PlayerTank1"))
+        {
+            GameObject expl = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
+            Destroy(expl, 1); // delete the explosion after 3 seconds
+            Destroy(gameObject, Impactdelay);
+            IsDestroyed = true;
+        }
+        else if (other.gameObject.CompareTag("PlayerTank2"))
+        {
+
+            GameObject expl = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
+            Destroy(expl, 1); // delete the explosion after 3 seconds
+            Destroy(gameObject, Impactdelay);
+            IsDestroyed = true;
+>>>>>>> Stashed changes
+        }
+
+ 
+
     }
+
+
+    void TileDestructionLoop (Vector3 impactLocation)
+    {
+            impactLocation.y = impactLocation.y + (float).3;
+            float StartVAR = impactLocation.x = impactLocation.x - (float).3;
+            tilemap.SetTile(tilemap.WorldToCell(impactLocation), null);
+        for (int i = 0; i < 6; i++)
+        {
+            for (int n = 0; n < 6; n++)
+            {
+                impactLocation.x = impactLocation.x + (float).1;
+                tilemap.SetTile(tilemap.WorldToCell(impactLocation), null);
+            }
+
+            impactLocation.y = impactLocation.y - (float).1;
+            impactLocation.x = StartVAR;
+            tilemap.SetTile(tilemap.WorldToCell(impactLocation), null);
+        }
+    }
+            
+            
+
+
+
     
 }
+
+    
+
 
